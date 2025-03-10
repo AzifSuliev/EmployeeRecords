60056d21b06a64e99fd06c10d4d4e4053ca82860
azif suliev <azif.suliev.89@list.ru>
2025-03-10 18:56:02 +0600

    pagination

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using EmployeeRecords.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace EmployeeRecords.Controllers
 {
@@ -18,9 +19,25 @@ namespace EmployeeRecords.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetAllEmployees(int? pageNumber, int? pageSize)
         {
-            return Ok(await _dbContext.Employees.ToListAsync());
+            int currentPageNumber = pageNumber ?? 1; // если pageNumber == null то currentPageNumber = 1
+            int currentPageSize = pageSize ?? 3; // если pageSize == null то currentPageSize = 3
+
+            var employeesFromDb = await (from employee in _dbContext.Employees
+                                         select new
+                                         {
+                                             Id = employee.Id,
+                                             FirstName = employee.FirstName,
+                                             LastName = employee.LastName,
+                                             Gender = employee.Gender,
+                                             HireDate = employee.HireDate,
+                                             PositionId = employee.PositionId,
+                                             DepartmentId = employee.DepartmentId
+                                         }).ToListAsync();
+
+            // Pagination
+            return Ok(employeesFromDb.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize));
         }
 
         [HttpGet("{id}")]
@@ -29,6 +46,42 @@ namespace EmployeeRecords.Controllers
             Employee employee = await _dbContext.Employees.FindAsync(id);
             if (employee == null) return NotFound("No record has been found against this ID");
             return Ok(employee);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> SearchEmployees(string query)
+        {
+            var employees = await (from employee in _dbContext.Employees
+                                   where employee.FirstName.Contains(query) || employee.LastName.Contains(query)
+                                   select new
+                                   {
+                                       Id = employee.Id,
+                                       FirstName = employee.FirstName,
+                                       LastName = employee.LastName,
+                                       Gender = employee.Gender,
+                                       HireDate = employee.HireDate,
+                                       PositionId = employee.PositionId,
+                                       DepartmentId = employee.DepartmentId
+                                   }).ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetNewEmployees()
+        {
+            var newEmployees = await (from employee in _dbContext.Employees
+                                      orderby employee.HireDate descending
+                                      select new
+                                      {
+                                          Id = employee.Id,
+                                          FirstName = employee.FirstName,
+                                          LastName = employee.LastName,
+                                          Gender = employee.Gender,
+                                          HireDate = employee.HireDate,
+                                          PositionId = employee.PositionId,
+                                          DepartmentId = employee.DepartmentId
+                                      }).Take(3).ToListAsync();
+            return Ok(newEmployees);
         }
 
         [HttpPost]
